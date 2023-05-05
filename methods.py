3a783c21336a54163c259c430fba43117a864152
Cecilia Isola Garcia <isola_cecilia@hotmail.com>
2023-05-05 09:11:22 -0300

    aula 9 -off-by: Cecilia Isola Garcia <isola_cecilia@hotmail.com>

--- methods.py
+++ methods.py
@@ -5,3 +5,19 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def sum (value1, value2):
+    sum = value1 + value2
+    return sum
+
+def sub(value1,value2):
+    sub = value1 - value2
+    return sub
+
+def mult(value1,value2):
+    mult = value1 * value2
+    return mult
+
+def div(value1,value2):
+    div = value1 / value2
+    return div
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,39 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_sum ():
+    #given two values 
+    value1 = 2
+    value2 = 5
+    #when we calculate the sum
+    output = methods.sum(value1,value2)
+    #then the sum should be 7
+    assert output == 7
+
+def test_subtraction():
+    #given two values
+    value1 = 10
+    value2 = 5
+    #when we subtract
+    output = methods.sub(value1,value2)
+    #then the subtract should be
+    assert output == 5
+
+def test_mult():
+    #given two values
+    value1 = 10
+    value2 = 5
+    #when we multiply
+    output = methods.mult(value1,value2)
+    #then the multiply should be
+    assert output == 50
+
+def test_div():
+    #given two values
+    value1 = 10
+    value2 = 5
+    #when we divide
+    output = methods.div(value1,value2)
+    #then the division should be
+    assert output == 2
